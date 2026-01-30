5897613300b579214064b578c7dead2d3397831e
blendonl <blendonluta@gmail.com>
2026-01-30 21:20:25 +0100

    auto-create agenda if missing when creating agenda item

--- apps/backend/src/core/agenda-item/usecase/agenda-item.create.usecase.ts
+++ apps/backend/src/core/agenda-item/usecase/agenda-item.create.usecase.ts
@@ -5,15 +5,29 @@ import {
   AGENDA_ITEM_REPOSITORY,
   type AgendaItemRepository,
 } from '../repository/agenda-item.repository';
+import {
+  AGENDA_REPOSITORY,
+  type AgendaRepository,
+} from '../../agenda/repository/agenda.repository';
 
 @Injectable()
 export class AgendaItemCreateUseCase {
   constructor(
     @Inject(AGENDA_ITEM_REPOSITORY)
     private readonly agendaItemRepository: AgendaItemRepository,
+    @Inject(AGENDA_REPOSITORY)
+    private readonly agendaRepository: AgendaRepository,
   ) {}
 
   async execute(agendaId: string, data: AgendaItemCreateData): Promise<AgendaItem> {
+    let agenda = await this.agendaRepository.findById(agendaId);
+
+    if (!agenda) {
+      const agendaDate = data.startAt || new Date();
+      agenda = await this.agendaRepository.create({ date: agendaDate });
+      agendaId = agenda.id;
+    }
+
     return this.agendaItemRepository.create(agendaId, data);
   }
 }
